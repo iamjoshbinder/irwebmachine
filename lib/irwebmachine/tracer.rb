72babddf07c545c39b4fa0a5a62797c0acf57df0
Rob Gleeson <rob@flowof.info>
2012-04-06 12:42:46 +0100

    No more blind debugging.

--- lib/irwebmachine/tracer.rb
+++ lib/irwebmachine/tracer.rb
@@ -10,7 +10,7 @@ class IRWebmachine::Tracer
   end
 
   def trace!
-    set_trace_func(tracer) 
+    set_trace_func method(:tracer).to_proc
     yield
     set_trace_func(nil)
   end
@@ -29,18 +29,32 @@ class IRWebmachine::Tracer
 
 private
 
-  def tracer
-    @tracer ||= lambda do |event, file, lineno, _, binding, receiver|
+  def tracer(event, file, lineno, id, binding, receiver) 
+    begin
       has_ancestor = @targets.any? do |t| 
         receiver.is_a?(Module) && receiver.ancestors.include?(t) 
       end
 
       if has_ancestor && @events.include?(event)
         stack << IRWebmachine::Frame.new(file, lineno, event, binding)
-        action = catch(:tracer) { @on_event.call(stack.last, stack.size-1) if @on_event }
+        action = catch(:tracer) { @on_event.call(stack.last, stack.size) if @on_event }
         set_trace_func(nil) if action == :stop
       end
+    rescue Exception => e
+      print_error(e)
+      set_trace_func(nil)
     end
   end
 
+  def print_error(e)
+    puts <<-CRASH
+
+      -- CRASH (IRWebmachine) -- 
+      
+      Exception => #{e.class}
+      Reason    => #{e.message}
+      Backtrace => #{e.backtrace.join "\n"}
+    
+    CRASH
+  end
 end
