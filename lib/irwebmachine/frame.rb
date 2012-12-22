8b6b056d169f5ef48548d4199637ec19b31ece58
Robert Gleeson <rob@flowof.info>
2012-12-22 01:25:46 +0100

    Refactor IRWebmachine::Frame.

--- lib/irwebmachine/frame.rb
+++ lib/irwebmachine/frame.rb
@@ -1,14 +1,11 @@
 class IRWebmachine::Frame
-
-  attr_reader :klass, :method, :binding, :file, :lineno
-
-  def initialize(file, lineno, event,binding)
-    @file    = file
-    @lineno  = lineno
+  def initialize(binding,event)
     @binding = binding
-    @method  = binding.eval("__method__")
-    @klass = binding.eval("self").method(@method).owner 
-    @event = event
+    @event   = event
+    @file    = binding.eval "__FILE__"
+    @lineno  = binding.eval "__LINE__"
+    @method  = binding.eval "__method__"
+    @klass   = binding.eval("self").method(@method).owner
   end
 
   def event?(type)
@@ -18,5 +15,4 @@ class IRWebmachine::Frame
   def to_s
     "#{@klass}##{@method}"
   end
-
 end
--- lib/irwebmachine/tracer.rb
+++ lib/irwebmachine/tracer.rb
@@ -1,7 +1,7 @@
 require 'thread'
 
 class IRWebmachine::Tracer
-  
+
   def initialize
     @thread  = nil
     @queue   = SizedQueue.new(1)
@@ -13,10 +13,10 @@ class IRWebmachine::Tracer
     @events.push(*event)
   end
 
-  def add_target *target 
+  def add_target *target
     @targets.push(*target)
   end
- 
+
   def finished?
     [false, nil].include? @thread.status
   end
@@ -34,20 +34,22 @@ class IRWebmachine::Tracer
     Thread.new do
       Thread.current.set_trace_func method(:tracer).to_proc
       yield
-      Thread.current.set_trace_func(nil) 
+      Thread.current.set_trace_func(nil)
     end
   end
 
-private 
+private
 
   def tracer(event, file, lineno, id, binding, klass)
     try do
-      has_ancestor = @targets.any? do |t| 
-        klass.is_a?(Module) && klass.ancestors.include?(t) 
+      has_ancestor = @targets.any? do |t|
+        # This says, "binding.eval('self') references a class or module,
+        # and the event(f. ex ':call') came from one of the iterated
+        # targets".
+        klass.is_a?(Module) && klass.ancestors.include?(t)
       end
-  
       if has_ancestor && @events.include?(event)
-        frame = IRWebmachine::Frame.new(file, lineno, event, binding)
+        frame = IRWebmachine::Frame.new(binding,event)
         @queue.enq(frame)
       end
     end
@@ -60,14 +62,14 @@ private
       puts <<-CRASH
 
        ----------------------
-        CRASH (IRWebmachine) 
+        CRASH (IRWebmachine)
        ----------------------
 
-       The tracer appears to be locked in infinite 
-       recursion. This may be a bug in IRWebmachine, 
+       The tracer appears to be locked in infinite
+       recursion. This may be a bug in IRWebmachine,
        but your own code may be the cause.
 
-       The tracer will stop execution now, but the 
+       The tracer will stop execution now, but the
        stack up to the point of infinite recursion
        is retained for debugging.
 
@@ -75,12 +77,12 @@ private
       Thread.current.set_trace_func(nil)
     rescue Exception => e
       puts <<-CRASH
-        
+
        ----------------------
-        CRASH (IRWebmachine) 
+        CRASH (IRWebmachine)
        ----------------------
 
-       The tracer has crashed. 
+       The tracer has crashed.
        This is a bug in IRWebmachine.
        Please report to https://github.com/generalassembly/irwebmachine.
        Thanks!
